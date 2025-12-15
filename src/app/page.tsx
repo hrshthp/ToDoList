bc043d19645a54075e12078a5a4eeebfbadb1cde
Harshitha <harshithavijaydeep@gmail.com>
2025-12-15 14:43:21 +0530

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,62 @@
-import Image from "next/image";
+"use client";
+import { use, useState } from "react";
+import { BBH_Sans_Bartle } from "next/font/google";
+const BBH = BBH_Sans_Bartle({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+export default function todo() {
+  const [task, setTask] = useState("");
+  const [todo, setTodo] = useState<string[]>([]);
 
-export default function Home() {
+  const addTask = () => {
+    if (!task.trim()) return;
+    setTodo([...todo, task]);
+    setTask("");
+  };
+  const deleteTask = (index: number) => {
+    setTodo(todo.filter((_, i) => i !== index));
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <>
+      <div className="min-h-screen justify-center items-center flex  bg-[#fdf7f7]">
+        <div className="bg-[#fdf7f7] border  border-[#df6186] items-center max-w-md w-full shadow-[#e10945] shadow-2xs  rounded-4xl p-6">
+          <h1
+            className={`text-3xl font-extrabold my-3 text-[#e10945] justify-between ${BBH.className}`}
+          >
+            To-Do List
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+          <div className="flex gap-x-7 mb-3">
+            <input
+              placeholder="Add task..."
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+              onKeyDown={(e) => e.key == "Enter" && addTask()}
+              className="rounded-2xl focus:ring-0 focus:outline-[#df6186] w-full active:ring-[#df6186] p-2 bg-[] justify-between text-[#e45d85] border-[#e45d85]"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <button className="text-[#e45d85] ml-2" onClick={addTask}>
+              Add
+            </button>{" "}
+          </div>
+          <ul className="space-y-2">
+            {todo.map((todo, index) => (
+              <li
+                key={index}
+                className="flex justify-between my-0.75 text-[#df6186] space-y-2"
+              >
+                <span>{todo}</span>
+                <button
+                  className="text-red-700 text-sm "
+                  onClick={() => deleteTask(index)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
